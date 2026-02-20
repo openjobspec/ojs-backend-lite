4ecfcc05a7fd25920443178ab77d11b1ec8d901d
Jose David Baena <josedab@github.com>
2026-02-20 06:22:46 +0100

    fix: prevent race condition in concurrent dequeue

--- internal/core/events.go
+++ internal/core/events.go
@@ -1,44 +1,17 @@
 package core
 
-import "time"
-
-const (
-	EventJobStateChanged = "job.state_changed"
-	EventJobProgress     = "job.progress"
-	EventServerShutdown  = "server.shutdown"
+import (
+	common "github.com/openjobspec/ojs-go-backend-common/core"
 )
 
-type JobEvent struct {
-	EventType string `json:"event"`
-	JobID     string `json:"job_id"`
-	Queue     string `json:"queue"`
-	JobType   string `json:"type"`
-	From      string `json:"from,omitempty"`
-	To        string `json:"to,omitempty"`
-	Progress  int    `json:"progress,omitempty"`
-	Message   string `json:"message,omitempty"`
-	Timestamp string `json:"timestamp"`
-}
+const (
+	EventJobStateChanged = common.EventJobStateChanged
+	EventJobProgress     = common.EventJobProgress
+	EventServerShutdown  = common.EventServerShutdown
+)
 
-func NewStateChangedEvent(jobID, queue, jobType, from, to string) *JobEvent {
-	return &JobEvent{
-		EventType: EventJobStateChanged,
-		JobID:     jobID,
-		Queue:     queue,
-		JobType:   jobType,
-		From:      from,
-		To:        to,
-		Timestamp: FormatTime(time.Now()),
-	}
-}
+type JobEvent = common.JobEvent
+type EventPublisher = common.EventPublisher
+type EventSubscriber = common.EventSubscriber
 
-type EventPublisher interface {
-	PublishJobEvent(event *JobEvent) error
-	Close() error
-}
-
-type EventSubscriber interface {
-	SubscribeJob(jobID string) (<-chan *JobEvent, func(), error)
-	SubscribeQueue(queue string) (<-chan *JobEvent, func(), error)
-	SubscribeAll() (<-chan *JobEvent, func(), error)
-}
+var NewStateChangedEvent = common.NewStateChangedEvent
